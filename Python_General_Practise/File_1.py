7e7025c0a1ad133c7ea259185a98682e131ce770
moru1234 <56359845+moru1234@users.noreply.github.com>
2024-12-30 13:29:17 +0530

    Update File_1.py

--- Python_General_Practise/File_1.py
+++ Python_General_Practise/File_1.py
@@ -15,3 +15,15 @@ print(5/3)
 print(2**4)#Exponentiation - Raises the first number to the power of the second.
 print(5//4)#Floor Division - Divides the first number by the second and returns the integer quotient
 print(5%4)#Modulus  - Divides the first number by the second and returns the remainder.
+#____________________________________________________________
+#print(float.__doc__)
+#print(int.__doc__)
+
+print(int("1010",base =2)) # Output is 10
+s1 = ':dog:\n'
+s2 = "Hello World"
+s3 ="""Hello Google"""
+print(type(s1), type(s2),type(s3))
+print(s1,"\n",s2,"\n",s3,"\n")
+print(len(s1),len(s2),len(s3))
+#____________________________________________________________
