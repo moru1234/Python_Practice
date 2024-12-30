65d7824da5b55f452285428c27a5f38448b9895e
moru1234 <56359845+moru1234@users.noreply.github.com>
2024-12-30 14:46:46 +0530

    Update List_tupple_set_dic_Datatype.py

--- Python_General_Practise/List_tupple_set_dic_Datatype.py
+++ Python_General_Practise/List_tupple_set_dic_Datatype.py
@@ -1,4 +1,4 @@
-
+#__________________________List_________________________________________________________
 #list Data type
 l=['python',3,'in', 'one']
 print(type(l)) #type of l
@@ -27,3 +27,31 @@ del l[2]#deleted element at 2nd index
 print(l)
 
 print(l.index('pic'))# get index for element 'pic' from list
+
+#__________________________Tupple_________________________________________________________
+tp =(1,2,3,[4,5]) #tupple is immutable data type
+print(type(tp))
+
+print(len(tp))
+print(tp[2])#get element at index 2 from tupple tp
+
+#Key Learnings:
+#Tuples are immutable: You cannot add, remove, or reassign elements in a tuple directly.
+#Mutable elements in tuples: If a tuple contains a mutable object (like a list), you can modify the contents of that object.
+#Example:
+#tp = (1, 2, 3, [4, 5])
+#tp[3][1] = 6 modifies the list [4, 5] to [4, 6], but the tuple structure remains unchanged.
+#This demonstrates that tuple immutability applies only to its structure, not to the contents of its mutable elements.
+#tp[2]=10 # this will give me error 
+tp[3][1]=6 # though tupple is mutable but we will still be able to modify list inside tupple
+print(tp)
+
+tp=(1,) #Writing tp = (1,) does not modify the original tuple; it reassigns the variable tp to a new tuple (1,)
+print(tp)
+
+
+#assign multiple values
+v=(3,2,'a')
+(c,b,a) =v
+print(a,b,c)
+#__________________________Set_________________________________________________________
