bf1318881c0028145cba8d1791fd769e3ffeb774
moru1234 <56359845+moru1234@users.noreply.github.com>
2024-12-30 13:18:31 +0530

    Update File_1.py

--- Python_General_Practise/File_1.py
+++ Python_General_Practise/File_1.py
@@ -7,3 +7,11 @@ b=0x10
 print(type(b))
 c=complex(1,3)
 print(type(c))
+#__________________________________________
+print(1+1)
+print(2-2)
+print(3*3)
+print(5/3)
+print(2**4)#Exponentiation - Raises the first number to the power of the second.
+print(5//4)#Floor Division - Divides the first number by the second and returns the integer quotient
+print(5%4)#Modulus  - Divides the first number by the second and returns the remainder.
